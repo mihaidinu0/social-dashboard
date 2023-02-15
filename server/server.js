da94ed1e01597107dcda7ef75fb76ecf26f71eb7
mihaidinu0 <dinu.mihai.dinel@gmail.com>
2023-02-16 00:12:10 +0200

    Server returns static index.html when / is accessed

--- server/server.js
+++ server/server.js
@@ -3,17 +3,26 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 app.use(require("./routes/record"));
+
 // get driver connection
 const dbo = require("./db/conn");
- 
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
+
   });
   console.log(`Server is running on port: ${port}`);
 });
+
